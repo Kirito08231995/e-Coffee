b69625c3f57075f99bba7dcaec2ca7d756369ace
john ricky lapitan <johnrickyl@ymail.com>
2022-05-04 10:56:36 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\CoffeeCtrl;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',[CoffeeCtrl::class, 'index']);
